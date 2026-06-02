4eeae5584e0f7489949e8906afcfbb0a0e421a6f
Nishalan Govender <nish@ubundi.co.za>
2026-06-02 13:39:59 +0200

    chore: fix fm_control lint failures

--- src/fm_control/src/control_node.cpp
+++ src/fm_control/src/control_node.cpp
@@ -1,3 +1,17 @@
+// Copyright 2026 First Motive
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
 // Minimal control_node stub for fm_control.
 // Replace with the real ros2_control hardware interface.
 #include "rclcpp/rclcpp.hpp"
@@ -5,7 +19,8 @@
 class ControlNode : public rclcpp::Node
 {
 public:
-  ControlNode() : Node("control_node")
+  ControlNode()
+  : Node("control_node")
   {
     RCLCPP_INFO(this->get_logger(), "fm_control control_node up");
   }
