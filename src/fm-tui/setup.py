976b826f2c842e77d368872696310e13b6566962
Nishalan Govender <nish@ubundi.co.za>
2026-06-03 14:28:22 +0200

    fix: pin external repos and textual to exact versions

--- src/fm-tui/setup.py
+++ src/fm-tui/setup.py
@@ -10,7 +10,7 @@ setup(
         ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
         ("share/" + package_name, ["package.xml"]),
     ],
-    install_requires=["setuptools", "textual>=0.74"],
+    install_requires=["setuptools", "textual==0.74.0"],
     zip_safe=True,
     maintainer="First Motive",
     maintainer_email="nish@ubundi.co.za",
